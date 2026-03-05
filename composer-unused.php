9c625edc41b617f49fdca6edf2103ed291c62e1b
Konrad Michalik <km@move-elevator.de>
2026-03-05 14:07:18 +0100

    build: add named filter for typo3/cms-fluid in composer-unused configuration

--- composer-unused.php
+++ composer-unused.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
  */
 
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
+use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 
 return static function (Configuration $config): Configuration {
-    return $config;
+    return $config
+        ->addNamedFilter(NamedFilter::fromString('typo3/cms-fluid'));
 };
