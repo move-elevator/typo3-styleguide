db64e014e49dd0656944dcf9a9528390d2c1483b
Konrad Michalik <km@move-elevator.de>
2026-04-02 15:31:16 +0200

    feat: add CTypeIconIdentifier ViewHelper for icon resolution

---
+++ Classes/ViewHelpers/CTypeIconIdentifierViewHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_styleguide" TYPO3 CMS extension.
+ *
+ * (c) 2025-2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MoveElevator\Styleguide\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * CTypeIconIdentifierViewHelper.
+ *
+ * Resolves a CType identifier to its registered icon identifier.
+ *
+ * @author Konrad Michalik <km@move-elevator.de>
+ * @license GPL-2.0-or-later
+ */
+class CTypeIconIdentifierViewHelper extends AbstractViewHelper
+{
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('ctype', 'string', 'CType identifier', true);
+    }
+
+    public function render(): string
+    {
+        $ctype = (string) $this->arguments['ctype'];
+        if ('' === $ctype) {
+            return '';
+        }
+
+        return (string) ($GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes'][$ctype] ?? '');
+    }
+}
